1b0a02cb774ef48e589aff50e56d738d0b2db1e0
VirgilMicu <virgil.micu@gmail.com>
2026-05-11 21:20:38 +0300

    Create linear_search.py

---
+++ src/linear_search.py
@@ -0,0 +1,14 @@
+def func (vect, key):
+    if not isinstance(key, int):
+        return -1
+    if not isinstance(vect, list):
+        return -1
+    if len(vect) != 5:
+        return -1
+    for i in range (0,5):
+        if not isinstance(vect[i], int):
+            return -1
+    for i in range (0,5):
+        if vect[i] == key:
+            return i
+    return -1
